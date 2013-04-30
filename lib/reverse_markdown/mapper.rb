4b75e9cd84e745d32d9221cdd1e6c6277e324800
Ben Axnick <ben.axnick@sitepointgroup.com>
2013-04-30 18:45:01 +1000

    Strip leading and trailing whitespace for bold and emphasis

--- lib/reverse_markdown/mapper.rb
+++ lib/reverse_markdown/mapper.rb
@@ -57,7 +57,9 @@ module ReverseMarkdown
 
         markdown_chunks = element.children.map { |child| process_element(child) }
         remove_adjacent_whitespace!(markdown_chunks)
-        output << markdown_chunks.join
+        incoming = markdown_chunks.join
+        incoming.strip! if [:b, :strong, :em, :i].include? element.name.to_sym
+        output << incoming
 
         output << ending(element).to_s
       end
